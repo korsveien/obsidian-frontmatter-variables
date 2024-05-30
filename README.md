77f3574ea34583f3d39fa5c1cd5480761bc7dbac
Peder Korsveien <peder.korsveien@bekk.no>
2024-05-30 17:42:58 +0200

    Some formatting

--- README.md
+++ README.md
@@ -15,5 +15,9 @@ Deploy to {{env}}.
 
 This plugin uses the MarkdownPostProcessor to convert the placeholders into the property values defined in the frontmatter when switching to Reading Mode.
 
+### Development
+
+
 ### TODO
 - [ ] Fix support for language code blocks
+- [ ] Fix bug where lines are repeated when a blank line is present between checklist items
--- main.ts
+++ main.ts
@@ -1,121 +1,117 @@
-import {
-  App,
-  MarkdownPostProcessorContext,
-  Plugin,
-  PluginSettingTab,
-  Setting,
-} from "obsidian";
-import { codePointSize } from "@codemirror/state";
-
-// Remember to rename these classes and interfaces!
+import { App, MarkdownPostProcessorContext, Plugin, PluginSettingTab, Setting, } from 'obsidian';
 
 interface Settings {
-  mySetting: string;
+	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: Settings = {
-  mySetting: "default",
+	mySetting: "default",
 };
 
 export default class FrontmatterVariablesPlugin extends Plugin {
-  settings: Settings;
+	settings: Settings;
 
-  async onload() {
-    await this.loadSettings();
+	async onload() {
+		await this.loadSettings();
 
-    const regex = /{{\s?\$frontmatter\.([a-zA-z-_]+)\s?}}/g;
+		const regex = /{{\s?\$frontmatter\.([a-zA-z-_]+)\s?}}/g;
 
-    this.registerMarkdownPostProcessor((element, context) => {
-      ["p", "h1", "h2", "h3", "h4", "h5", "h6", "code"].forEach((selector) => {
-        if (selector === "code") {
-          element.findAll(selector).forEach((codeblock) => {
-            const text = codeblock.innerText = codeblock.innerText.trim();
+		this.registerMarkdownPostProcessor((element, context) => {
+			["p", "h1", "h2", "h3", "h4", "h5", "h6", "code"].forEach((selector) => {
+				if (selector === "code") {
+					element.findAll(selector).forEach((codeblock) => {
+						const text = codeblock.innerText = codeblock.innerText.trim();
 
-            const newText = text.replace(
-              regex,
-              this.replacePlaceholder(context),
-            );
+						const newText = text.replace(
+							regex,
+							this.replacePlaceholder(context),
+						);
 
-            const newSpan = codeblock.createSpan({ text: newText });
-            codeblock.replaceWith(newSpan);
-          });
-        } else {
-          element.findAll(selector).forEach((element) => {
-            element.innerText = element.innerText.trim().replace(
-              regex,
-              this.replacePlaceholder(context),
-            );
-          });
-        }
-      });
+						const newSpan = codeblock.createSpan({ text: newText });
+						codeblock.replaceWith(newSpan);
+					});
+				} else {
+					element.findAll(selector).forEach((element) => {
+						element.innerText = element.innerText.trim().replace(
+							regex,
+							this.replacePlaceholder(context),
+						);
+					});
+				}
+			});
 
-      ["li"].forEach((selector) => {
-        element.findAll(selector).forEach((element) => {
-          element.findAll("code").forEach((codeblock) => {
-            const text = codeblock.innerText = codeblock.innerText.trim();
+			["li"].forEach((selector) => {
+				element.findAll(selector).forEach((element) => {
+					element.findAll("code").forEach((codeblock) => {
+						const text = codeblock.innerText = codeblock.innerText.trim();
 
-            const newText = text.replace(
-              regex,
-              this.replacePlaceholder(context),
-            );
+						const newText = text.replace(
+							regex,
+							this.replacePlaceholder(context),
+						);
 
-            const newCodeBlock = codeblock.createEl(
-              "code",
-              {
-                text: newText,
-              },
-            );
-            codeblock.replaceWith(newCodeBlock);
-          });
-        });
-      });
-    });
+						const newCodeBlock = codeblock.createEl(
+							"code",
+							{
+								text: newText,
+							},
+						);
+						codeblock.replaceWith(newCodeBlock);
+					});
+				});
+			});
+		});
 
-    // This adds a settings tab so the user can configure various aspects of the plugin
-    this.addSettingTab(new SampleSettingTab(this.app, this));
-  }
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
 
-  private replacePlaceholder(context: MarkdownPostProcessorContext) {
-    return (_: string, word: string) =>
-      context.frontmatter[word] ?? "{{NO_VALUE}}";
-  }
+	private replacePlaceholder(context: MarkdownPostProcessorContext) {
+		return (_: string, word: string) =>
+			context.frontmatter[word] ?? "{{NO_VALUE}}";
+	}
 
-  onunload() {
-  }
+	onunload() {
+	}
 
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
 
-  async saveSettings() {
-    await this.saveData(this.settings);
-  }
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-  plugin: FrontmatterVariablesPlugin;
+	plugin: FrontmatterVariablesPlugin;
 
-  constructor(app: App, plugin: FrontmatterVariablesPlugin) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
+	constructor(app: App, plugin: FrontmatterVariablesPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
 
-  display(): void {
-    const { containerEl } = this;
+<<<<<<< HEAD
+	display(): void {
+		const { containerEl } = this;
+=======
+	display(): void {
+		const { containerEl } = this;
+>>>>>>> 2f6dbf3 (Some formatting)
 
-    containerEl.empty();
+		containerEl.empty();
 
-    new Setting(containerEl)
-      .setName("Setting #1")
-      .setDesc("It's a secret")
-      .addText((text) =>
-        text
-          .setPlaceholder("Enter your secret")
-          .setValue(this.plugin.settings.mySetting)
-          .onChange(async (value) => {
-            this.plugin.settings.mySetting = value;
-            await this.plugin.saveSettings();
-          })
-      );
-  }
+		new Setting(containerEl)
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
 }
